4a25d1656016e658042efd7d9513d0456945a419
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-07-12 11:58:44 +0400

    Sugar

--- src/middleware.js
+++ src/middleware.js
@@ -6,30 +6,27 @@ import {
 import { ConnectionManager } from './lib'
 
 export default function createWebsocketMiddleware (options = {}) {
-  return function (store) {
+  return store => {
     const connections = new ConnectionManager(store, options)
+    return next => action => {
+      if (!isSocketAction(action) || isIncomingMessage(action)) {
+        return next(action)
+      }
 
-    return function (next) {
-      return function (action) {
-        if (!isSocketAction(action) || isIncomingMessage(action)) {
-          return next(action)
-        }
+      const endpoint = action.meta.socket
 
-        const endpoint = action.meta.socket
+      const connection = connections.has(endpoint)
+        ? connections.get(endpoint)
+        : connections.add(endpoint)
 
-        const connection = connections.has(endpoint)
-          ? connections.get(endpoint)
-          : connections.add(endpoint)
-
-        if (connection) {
-          connection.socket.send(action.payload)
-        } else {
-          console.warn(`
+      if (connection) {
+        connection.socket.send(action.payload)
+      } else {
+        console.warn(`
             You haven't set up default endpoint
             Or the endpoint you're trying to use
             Is not connected: ${endpoint}
           `)
-        }
       }
     }
   }
