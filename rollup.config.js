09ea7e4a42efba32f1e7a1b324cdc8ddc81711d2
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-07-12 11:53:39 +0400

    added build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,42 @@
-import buble from 'rollup-plugin-buble'
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
 
-const pkg = require('./package.json')
+var env = process.env.NODE_ENV
+var pkg = require('./package.json')
 
-export default {
+var config = {
   entry: 'src/index.js',
-  plugins: [buble()],
   sourceMap: true,
+  plugins: [
+    resolve({
+      jsnext: true
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
   targets: [
     {
       dest: pkg['main'],
       format: 'umd',
       moduleName: 'ReduxWebsocketMiddleware',
       exports: 'named'
-    },
-    {
-      dest: pkg['jsnext:main'],
-      format: 'es'
     }
   ]
 }
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
