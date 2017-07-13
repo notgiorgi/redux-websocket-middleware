6207c9173114117ded65df882758743ef10b0568
Giorgi Bagdavadze <notgiorgi@gmail.com>
2017-07-13 18:05:03 +0400

    export base codecs

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import createWebsocketMiddleware from './middleware'
 export { ActionTypes } from './actions'
 
 export { createWebsocketMiddleware }
+export { JSONCodec, PlainTextCodec } from './lib'
 export default createWebsocketMiddleware
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,5 @@
 export { default as Connection } from './Connection'
 export { default as MessageQueue } from './MessageQueue'
 export { default as ConnectionManager } from './ConnectionManager'
+
+export { JSONCodec, PlainTextCodec } from './Codecs'
